f8b3342cfe0f001de024ba8121e3b26d3a2f19c7
Kacper Marcisz <kacpermarcisz@gmail.com>
2018-05-01 17:14:12 +0200

    init comit

---
+++ app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var passport = require('./utils/passport');
+passport(app);
+
+app.use(function(req, res, next) {
+  if(req.isAuthenticated()){
+    //if user is looged in, req.isAuthenticated() will return true
+    res.locals.logInfo = `Zalogowany: ${req.user.username}`;
+    next();
+  } else{
+    res.locals.logInfo = `Niezalogowany`;
+    if(req.url==='/' || req.url==='/sesja' || req.url==='/login' || req.url==='/users/reset' ) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  }
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;``
